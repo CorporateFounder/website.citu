b34621ef7ca7a263d8f1a06025787a5a38ed947e
0x258 <136977916@qq.com>
2023-08-05 04:51:46 +0800

    Make official website

--- src/web/src/App.vue
+++ src/web/src/App.vue
@@ -1,5 +1,153 @@
 <template>
-	<div class="container mx-auto px-4 flex h-screen items-center justify-center">{{ $t('msg.$home') }}</div>
+	<div class="">
+		<div class="header__bg">
+			<!-- <div class="header"></div> -->
+			<div class="header__describe">
+				<!-- <h2>Summary and Benefits</h2> -->
+				<p>
+					A completely unique project that has no analogues. A unique algorithm that made it possible to get rid of halving and at the same time maintain
+					<span>
+						a limit on the number of coins by burning 0.2% of digital dollars from all accounts every six months and 0.4% of digital shares every six months.
+					</span>
+				</p>
+			</div>
+		</div>
+		<div class="bg">
+			<div class="citu__message">
+				<div class="citu__message__title">Coin Info</div>
+				<div class="citu__message__row" style="margin-top: 70px">
+					<div class="border citu__message__item">
+						<div class="dot"></div>
+						<div class="y"></div>
+						<div class="message__box">
+							<div class="message__box__title">Coin Name</div>
+							<div class="message__box__describe">CITU</div>
+						</div>
+					</div>
+					<div class="border citu__message__item">
+						<div class="dot"></div>
+						<div class="y"></div>
+						<div class="message__box">
+							<div class="message__box__title">区块时间</div>
+							<div class="message__box__describe">150秒（每种算法5分钟）</div>
+						</div>
+					</div>
+					<div class="border citu__message__item">
+						<div class="dot"></div>
+						<div class="y"></div>
+						<div class="message__box">
+							<div class="message__box__title">区块奖励</div>
+							<div class="message__box__describe">动态 400 CITU 最高可达区块链限额</div>
+						</div>
+					</div>
+				</div>
+				<div class="citu__message__row" style="margin-top: 40px">
+					<div class="border citu__message__item">
+						<div class="dot"></div>
+						<div class="y"></div>
+						<div class="message__box">
+							<div class="message__box__title">每天产出</div>
+							<div class="message__box__describe">230400/CITU</div>
+						</div>
+					</div>
+					<div class="border citu__message__item row-span-2">
+						<div class="dot"></div>
+						<div class="y"></div>
+						<div class="message__box">
+							<div class="message__box__title">POW算法</div>
+							<div class="message__box__describe">CITU（独一无二，完全团队开法算法，具有超级私密和安全性）</div>
+						</div>
+					</div>
+				</div>
+				<div class="citu__message__row" style="margin-top: 40px">
+					<div class="border citu__message__item row-span-2">
+						<div class="dot"></div>
+						<div class="y"></div>
+						<div class="message__box">
+							<div class="message__box__title">总量</div>
+							<div class="message__box__describe">2,000,000,000</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="citu__describe">
+				<div class="citu__describe__title">
+					CITU
+					一个完全独特的项目，一种独特的算法是一种创新的支付系统，允许任何能够访问互联网的人向世界上任何地方的任何人接收或付款开采得时候产生可以美元和美元美元流通交易，股票具有社区投票功能和后期功能扩展上。
+				</div>
+				<div class="citu__describe__info">
+					<div>
+						<div class="describe__info__left">
+							<div class="left__title">
+								<div class="left__title--icon"></div>
+								<div class="left__title--text">特点</div>
+							</div>
+							<div class="left__content">
+								<p>CITU 结合了互联网安全功能，可保护您免受网络钓鱼尝试、受恶意软件感染的网站和其他网络威胁。它确保整体安全的浏览体验。</p>
+
+								<p>
+									我们的目标是提供一种无缝集成的解决方案，涵盖恶意软件防护、云服务、互联网安全、驱动程序扫描和更新、密钥和密码库、远程设备访问、安全加密钱包和系统优化——全部集中在一个地方。
+								</p>
+
+								<p>
+									CITU 基于密码学、去中心化和共识原则，确保交易可信,分散。
+									CITU的愿景是通过使用最先进的CITU方法创建最佳的隐私和安全货币，为这个世界提供一个更好的去中心化系统，消除当前的隐私，金融体系垄断和经济不平衡问题。所有节点都具有相同的权力和控制力;没有节点比其他节点具有更大的影响力，即主节点。货币不是由任何个人或公司（即中央机构）创建、维护或代表的。
+								</p>
+							</div>
+						</div>
+					</div>
+					<div>
+						<div class="describe__info__left">
+							<div class="left__title">
+								<div class="left__title--icon community"></div>
+								<div class="left__title--text">社区驱动</div>
+							</div>
+							<div class="left__content">
+								<p>由社区爱好者推动。当有很多不被支配的成员时，这是真正的去中心化。您可以作为开发人员或支持者加入我们。</p>
+							</div>
+						</div>
+						<div class="describe__info__left" style="margin-top: 40px">
+							<div class="left__title">
+								<div class="left__title--icon safety"></div>
+								<div class="left__title--text">安全</div>
+							</div>
+							<div class="left__content">
+								<p>没有隔离见证，没有RBF，代码中没有其他奇怪的东西。 简而言之，CITU是真正的私人数字现金。这是金钱的未来。</p>
+							</div>
+						</div>
+						<div class="describe__info__left" style="margin-top: 40px">
+							<div class="left__title">
+								<div class="left__title--icon disperse"></div>
+								<div class="left__title--text">分散</div>
+							</div>
+							<div class="left__content">
+								<p>
+									CITU的愿景是通过使用最先进的CITU方法创建最佳的隐私和安全货币，为这个世界提供一个更好的去中心化系统，消除当前的隐私，金融体系垄断和经济不平衡问题。所有节点都具有相同的权力和控制力;没有节点比其他节点具有更大的影响力，即主节点。货币不是由任何个人或公司（即中央机构）创建、维护或代表的。
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="box">
+			<div class="box_content">
+				<div class="logo">
+					<img src="./static/icon/logo.png" alt="" srcset="" />
+				</div>
+				<div class="box_content_text">
+					CITU旨在通过零缺陷/零币实施来减少或消除比特币和其他隐私币在市场上使用的现有隐私问题。任何人都可以查看公共钱包，不仅包括钱包的余额，还包括已收到和支付的金额（包括发送者和接收者的公共钱包地址）。CITU
+					通过引入铸币场景为用户提供了保持匿名或不保持匿名的自由，用户可以以稳健和分散的方式决定匿名程度。仅“铸币和支出”功能就在需要时为 CITU
+					本身带来了匿名性，而不是像目前这样通过公共分类账开放查看所有内容
+				</div>
+			</div>
+		</div>
+		<div class="footer">
+			<img src="./static/icon/t.png" alt="" />
+			<img src="./static/icon/tg.png" alt="" />
+			<img src="./static/icon/git.png" alt="" />
+		</div>
+	</div>
 </template>
 
 <script>
