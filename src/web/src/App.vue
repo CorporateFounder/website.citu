d540be793f31d3073e56365c5e7c07edc6bfc294
ASUS <tuychievnetmat@gmail.com>
2023-08-05 21:22:32 +0500

    change text

--- src/web/src/App.vue
+++ src/web/src/App.vue
@@ -5,8 +5,8 @@
 			<div class="header__describe">
 				<!-- <h2>Summary and Benefits</h2> -->
 				<p>
-					A completely unique project that has no analogues. A unique algorithm that made it possible to get rid of halving and at the same time maintain
 					<span>
+						A completely unique project that has no analogues. A unique algorithm that made it possible to get rid of halving and at the same time maintain
 						a limit on the number of coins by burning 0.2% of digital dollars from all accounts every six months and 0.4% of digital shares every six months.
 					</span>
 				</p>
@@ -28,16 +28,16 @@
 						<div class="dot"></div>
 						<div class="y"></div>
 						<div class="message__box">
-							<div class="message__box__title">区块时间</div>
-							<div class="message__box__describe">150秒（每种算法5分钟）</div>
+							<div class="message__box__title">block time</div>
+							<div class="message__box__describe">block is located in 150 seconds</div>
 						</div>
 					</div>
 					<div class="border citu__message__item">
 						<div class="dot"></div>
 						<div class="y"></div>
 						<div class="message__box">
-							<div class="message__box__title">区块奖励</div>
-							<div class="message__box__describe">动态 400 CITU 最高可达区块链限额</div>
+							<div class="message__box__title">block reward</div>
+							<div class="message__box__describe">For one block, 400 digital dollars and shares are given</div>
 						</div>
 					</div>
 				</div>
@@ -46,7 +46,7 @@
 						<div class="dot"></div>
 						<div class="y"></div>
 						<div class="message__box">
-							<div class="message__box__title">每天产出</div>
+							<div class="message__box__title">day out</div>
 							<div class="message__box__describe">230400/CITU</div>
 						</div>
 					</div>
@@ -54,8 +54,8 @@
 						<div class="dot"></div>
 						<div class="y"></div>
 						<div class="message__box">
-							<div class="message__box__title">POW算法</div>
-							<div class="message__box__describe">CITU（独一无二，完全团队开法算法，具有超级私密和安全性）</div>
+							<div class="message__box__title">POW algorithm</div>
+							<div class="message__box__describe">CITU(Unique, fully command algorithm with super privacy and security)</div>
 						</div>
 					</div>
 				</div>
@@ -64,8 +64,11 @@
 						<div class="dot"></div>
 						<div class="y"></div>
 						<div class="message__box">
-							<div class="message__box__title">总量</div>
+							<div class="message__box__title">Total dollars</div>
 							<div class="message__box__describe">2,000,000,000</div>
+
+							<div class="message__box__title">Total stock</div>
+                            <div class="message__box__describe">1,000,000,000</div>
 						</div>
 					</div>
 				</div>
@@ -73,8 +76,7 @@
 			<div class="citu__describe">
 				<div class="citu__describe__title">
 					CITU
-					一个完全独特的项目，一种独特的算法是一种创新的支付系统，允许任何能够访问互联网的人向世界上任何地方的任何人接收或付款开采得时候产生可以美元和美元美元流通交易，股票具有社区投票功能和后期功能扩展上。
-				</div>
+                    A unique project built completely from scratch on a completely new architecture, only in this system there is a unique electoral system, as well as coin restrictions without halving. The ability to send money to anywhere in the world in two minutes.				</div>
 				<div class="citu__describe__info">
 					<div>
 						<div class="describe__info__left">
@@ -83,15 +85,17 @@
 								<div class="left__title--text">特点</div>
 							</div>
 							<div class="left__content">
-								<p>CITU 结合了互联网安全功能，可保护您免受网络钓鱼尝试、受恶意软件感染的网站和其他网络威胁。它确保整体安全的浏览体验。</p>
+								<p>CITU combines internet security features to protect you from phishing attempts, malware-infected websites and other online threats. This provides an overall secure browsing experience.</p>
 
 								<p>
-									我们的目标是提供一种无缝集成的解决方案，涵盖恶意软件防护、云服务、互联网安全、驱动程序扫描和更新、密钥和密码库、远程设备访问、安全加密钱包和系统优化——全部集中在一个地方。
+									Our goal is to provide a fully integrated solution covering malware protection, cloud services, internet security, driver scanning and updating, key and password storage, remote device access, secure encrypted wallet and system optimization - all in one place.
 								</p>
 
 								<p>
-									CITU 基于密码学、去中心化和共识原则，确保交易可信,分散。
-									CITU的愿景是通过使用最先进的CITU方法创建最佳的隐私和安全货币，为这个世界提供一个更好的去中心化系统，消除当前的隐私，金融体系垄断和经济不平衡问题。所有节点都具有相同的权力和控制力;没有节点比其他节点具有更大的影响力，即主节点。货币不是由任何个人或公司（即中央机构）创建、维护或代表的。
+
+									CITU is based on the principles of cryptography, decentralization and consensus to ensure the reliability and decentralization of transactions.
+                                    The vision of CITU is to provide the best decentralized system for this world, using the most advanced CITU methods to create the best privacy and security currency, eliminating the current issues of privacy, financial system monopoly and economic imbalance. All nodes have equal power and control; no node has more influence than other nodes, namely the main nodes. Currencies are not created, maintained or represented by any person or company (i.e. central authority).
+
 								</p>
 							</div>
 						</div>
@@ -100,10 +104,10 @@
 						<div class="describe__info__left">
 							<div class="left__title">
 								<div class="left__title--icon community"></div>
-								<div class="left__title--text">社区驱动</div>
+								<div class="left__title--text">community driven</div>
 							</div>
 							<div class="left__content">
-								<p>由社区爱好者推动。当有很多不被支配的成员时，这是真正的去中心化。您可以作为开发人员或支持者加入我们。</p>
+								<p>Driven by community enthusiasts. This is real decentralization when there are many non-dominated participants. You can join us as a developer or supporter.</p>
 							</div>
 						</div>
 						<div class="describe__info__left" style="margin-top: 40px">
@@ -112,7 +116,7 @@
 								<div class="left__title--text">安全</div>
 							</div>
 							<div class="left__content">
-								<p>没有隔离见证，没有RBF，代码中没有其他奇怪的东西。 简而言之，CITU是真正的私人数字现金。这是金钱的未来。</p>
+								<p>the code is completely open access to the code. At the same time, there are two types of coins at once (Shares and Dollars). A unique system of protection against inflation and high price fluctuations. You will no longer have the fear that tomorrow the miners will be reduced and the protection will fall.</p>
 							</div>
 						</div>
 						<div class="describe__info__left" style="margin-top: 40px">
@@ -122,8 +126,19 @@
 							</div>
 							<div class="left__content">
 								<p>
-									CITU的愿景是通过使用最先进的CITU方法创建最佳的隐私和安全货币，为这个世界提供一个更好的去中心化系统，消除当前的隐私，金融体系垄断和经济不平衡问题。所有节点都具有相同的权力和控制力;没有节点比其他节点具有更大的影响力，即主节点。货币不是由任何个人或公司（即中央机构）创建、维护或代表的。
-								</p>
+                            How will this system deal with high volatility?
+                            The burning of coins every six months will help to reduce the volatility of the currency. This is because it will create a built-in incentive for people to spend their money rather than hoard it. If people know that their money will be worth less in the future, they will be more likely to spend it now. This will help to keep the currency circulating and prevent it from becoming too volatile.
+                            How will this system deal with inflation?
+                            The limited supply of coins will help to prevent inflation. The total number of coins will never grow above 20 billion digital dollars and 10 billion digital shares. This means that the value of each coin will be relatively stable over time.
+                            How will this system deal with stagflation?
+                            Stagflation is a combination of high inflation and high unemployment. The burning of coins will help to prevent stagflation by stimulating economic activity. When people spend their money, they are more likely to buy goods and services from businesses. This will create jobs and help to reduce unemployment.
+                            How will this system deal with a deflationary spiral?
+                            A deflationary spiral is a situation where prices are falling and people are reluctant to spend money because they expect prices to fall even further. The burning of coins will help to prevent a deflationary spiral by creating an incentive for people to spend their money. If people know that their money will be worth less in the future, they will be more likely to spend it now. This will help to keep prices from falling and prevent a deflationary spiral.
+                            How will this coin help save money and increase the wealth of the owners of this coin?
+                            The burning of coins will help people to save money. This is because people will be less likely to hoard money if they know that it will be worth less in the future. Instead, they will be more likely to invest their money or spend it on goods and services. This will help to increase the wealth of the owners of this coin.
+                            In addition, the limited supply of coins will help to increase the value of each coin. This is because there will be a finite number of coins available, but the demand for coins may increase over time. This could lead to an increase in the value of each coin, which would benefit the owners of this coin.
+                            Overall, this system has the potential to address a number of economic problems, such as high volatility, inflation, stagflation, and deflationary spirals. It could also help people to save money and increase their wealth.
+                            </p>
 							</div>
 						</div>
 					</div>
@@ -136,9 +151,9 @@
 					<img src="./static/icon/logo.png" alt="" srcset="" />
 				</div>
 				<div class="box_content_text">
-					CITU旨在通过零缺陷/零币实施来减少或消除比特币和其他隐私币在市场上使用的现有隐私问题。任何人都可以查看公共钱包，不仅包括钱包的余额，还包括已收到和支付的金额（包括发送者和接收者的公共钱包地址）。CITU
-					通过引入铸币场景为用户提供了保持匿名或不保持匿名的自由，用户可以以稳健和分散的方式决定匿名程度。仅“铸币和支出”功能就在需要时为 CITU
-					本身带来了匿名性，而不是像目前这样通过公共分类账开放查看所有内容
+					CITU aims to reduce or eliminate existing privacy issues with bitcoin and other privacy coins used in the market through a bug-free implementation of Zerocoin. Anyone can view the public wallet, not only the wallet balance, but also the amount received and paid (including the sender and recipient addresses of the public wallets). CITU
+                    By implementing a minting script that gives users the freedom to remain anonymous or not, users can decide the degree of anonymity in a secure and decentralized manner. The mint and spend feature alone is CITU when needed.
+                    inherently provides anonymity, and is not open to viewing everything through a public registry, as is currently the case.
 				</div>
 			</div>
 		</div>
